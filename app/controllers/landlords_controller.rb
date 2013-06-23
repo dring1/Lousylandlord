824bcc49f3c733982c7332f7bbad1d459d62f9cb
dring1 <devon.ring@gmail.com>
2013-06-23 18:23:23 -0400

    Miracle work --- > comments now add properly

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -15,15 +15,21 @@ class LandlordsController < ApplicationController
   def create  	
     #check if a landlord of the same name already exists and add comments to that
   	@landlord = Landlord.where(:name => params[:landlord][:name], :city => params[:landlord][:city], :province => params[:landlord][:province]).first_or_create!
-    @landlord.comments.create if @landlord.comments.empty? 
-    @landlord.comments.[0].setIP request.remote_ip
-    @landlord.comments.[0].comment = params[:landlord][:comments_attributes]["0"][:comment]
+    @landlord.comments.create #if @landlord.comments.empty? 
+    @landlord.comments.last.setIP request.remote_ip
+    @landlord.comments.last.comment = params[:landlord][:comments_attributes]["0"][:comment]
+    @landlord.comments.last.terms = params[:landlord][:comments_attributes]["0"][:terms]
+    # @comment.setIP request.remote_ip
+    # @coment.comment = params[:landlord][:comments_attributes]["0"][:comment]
+    # @comment.terms = params[:landlord][:comments_attributes]["0"][:terms]
+
 
 
     if @landlord.save     
       flash[:success] = "Thank you for submitting a Landlord "
       redirect_to landlords_path
     else
+      #needs to display form errors
       flash[:fail] = "Failed to submit landlord"
       redirect_to landlords_path
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@ class Comment < ActiveRecord::Base
 	belongs_to :landlord
 
 	#validates :landlord_id, presence: true
-	#validates :ip, presence: true
-	#validates :terms, presence: true
+	validates :ip, presence: true
+	validates :terms, presence: true
     validates :comment, presence: true, 
 				length: {minimum: 15, maximum: 500}		
 
