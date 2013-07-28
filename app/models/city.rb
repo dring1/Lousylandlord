ffb1c7bb7bee740038ef826ede108791267c30db
dring1 <devon.ring@gmail.com>
2013-07-28 17:06:57 -0400

    added to scripts build city/province and list of cities

---
+++ app/models/city.rb
@@ -0,0 +1,4 @@
+class City < ActiveRecord::Base
+  attr_accessible :name, :province
+  belongs_to :province
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 20130728163620) do
+
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.string   "province"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "comments", :force => true do |t|
     t.text     "comment"
@@ -28,6 +35,12 @@ ActiveRecord::Schema.define(:version => 5) do
     t.string "name"
     t.string "city"
     t.string "province"
+  end
+
+  create_table "provinces", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
