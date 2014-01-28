876b6dddff0e202c51bd7e392762c0a7642e6df2
dring1 <devon.ring@gmail.com>
2014-01-27 20:49:32 -0500

    prod ssl false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ LousyLandLord::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -28,7 +28,7 @@ LousyLandLord::Application.configure do
    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
