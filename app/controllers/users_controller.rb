47c7fecdc6867d7a7d680446c866af2a9e920f8d
fredericmarchand <fred.marchand.23@gmail.com>
2014-05-03 10:41:51 -0400

    Removed other gitignore files and removed user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def new
-  	@user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
   end
