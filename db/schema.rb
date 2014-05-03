784056d213f3ce8a2c7e19f871e5742f1c2fd482
fredericmarchand <fred.marchand.23@gmail.com>
2014-05-03 10:37:07 -0400

    Removed databases from git repo and made email the primary key of users table, will need to rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422202912) do
+ActiveRecord::Schema.define(version: 20140503143221) do
 
   create_table "addresses", force: true do |t|
     t.string   "number"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20140422202912) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
